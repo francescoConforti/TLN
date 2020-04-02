b240d0c34356cf5c24dbc0d6fe0092890ba9395e
francescoConforti <francesco.conforti.04@gmail.com>
2020-04-02 16:19:05 +0200

    implemented lesk on sentences.txt; may improve results

--- esercizi_parte_2/WordSenseDisambiguation/wsd.py
+++ esercizi_parte_2/WordSenseDisambiguation/wsd.py
@@ -2,11 +2,34 @@ from nltk.corpus import wordnet
 import string
 import re
 
+# signature should already be a set
+def computeOverlap(signature, context):
+  return len(set(context).intersection(signature))
+
+# delete words which are not interesting for the context
+def removeNoise(bagOfWords):
+  return [word 
+          for word in bagOfWords
+          if (len(wordnet.synsets(word)) > 0) and (wordnet.synsets(word)[0].pos() in [wordnet.ADJ, wordnet.VERB, wordnet.NOUN, wordnet.ADV])]
+
 def simplifiedLesk(word, sentence):
   bestSense = wordnet.synsets(word)[0]
   maxOverlap = 0
   context = sentence.translate(sentence.maketrans('', '', string.punctuation)).split()
-  print(bestSense)
+  for sense in wordnet.synsets(word):
+    gloss = sense.definition()
+    examples = sense.examples()
+    signature = re.findall(r'\w+', gloss)
+    for example in examples:
+      signature = signature + re.findall(r'\w+', example)
+    signature = list(set(signature))
+    signature = removeNoise(signature)
+    overlap = computeOverlap(signature, context)
+    if overlap > maxOverlap:
+      maxOverlap = overlap
+      bestSense = sense
+  #print("overlap: " + str(maxOverlap))
+  return bestSense
 
 # sentences.txt
 myFile = open("./sentences.txt")
@@ -14,5 +37,11 @@ for sentence in myFile:
   if sentence[0] == "-":
     targetWord = re.search('\\*\\*(.+?)\\*\\*', sentence).group(1)
     targetWord = targetWord.translate(targetWord.maketrans('', '', '*'))
-    simplifiedLesk(targetWord, sentence)
+    sense = simplifiedLesk(targetWord, sentence)
+    sentenceHalves = sentence.split("**"+targetWord+"**")
+    print(sentenceHalves[0], end= " ")
+    for l in sense.lemmas():
+      print(l.name(), end="/")
+    print(" " + sentenceHalves[1] + "\t" + str(sense))
+
 myFile.close()
