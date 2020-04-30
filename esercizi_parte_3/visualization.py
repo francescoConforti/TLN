e11534a8ac16e80d43bce0adb62c52cecefacd4e
francescoConforti <francesco.conforti.04@gmail.com>
2020-04-30 16:48:48 +0200

    code for co occurrence matrix, fixed argparse

--- esercizi_parte_3/visualization.py
+++ esercizi_parte_3/visualization.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from string import punctuation
+from collections import OrderedDict
 import re
 import argparse
 import sentiment_analysis
@@ -35,9 +36,28 @@ def tokenize(text):
 
     return filtered_tokens
 
+# tokens is a list of tweets, each sublist is a list of tokens
+# i consider two tokens related if they appear in the same tweet
+def tokensToMatrix(tokens):
+    mat = OrderedDict()
+    tokenSet = []
+    for tweet in tokens:
+        for t in tweet:
+            tokenSet.append(t)
+    tokenSet = set(tokenSet)
+    for t1 in tokenSet:
+        mat[t1] = OrderedDict()
+    for t1 in tokenSet:
+        for t2 in tokenSet:
+            count = 0
+            for tweet in tokens:
+                if t1 != t2:
+                    if t1 in tweet and t2 in tweet:
+                        count += 1
+            mat[t1][t2] = count
+    return mat
+
 # take a list of tweets and make a wordcloud of it
-
-
 def plot_wordcloud(tweets, sentiment="neutral"):
     stopwords = set(STOPWORDS)
     text = " ".join(tweets)
@@ -74,10 +94,10 @@ def plot_wordcloud(tweets, sentiment="neutral"):
 def main():
     parser = argparse.ArgumentParser(
         description='Choose what tweets you want visualized, by which sentiment, and how')
-    parser.add_argument('-t', '--type', nargs=1, choices=['positive', 'negative', 'neutral', 'all'],
-                        default='all', help="What kind of tweets: positive, neutral, negative or all")
-    parser.add_argument('-v', '--visualization', nargs=1, choices=[
-                        'wordcloud', 'correlation_circle'], default='wordcloud', help="Type of visualization: wordcloud")
+    parser.add_argument('-t', '--type', nargs=1, choices=['positive', 'negative', 'neutral'],
+                        default=['all'], help="What kind of tweets: positive, neutral, negative or all")
+    parser.add_argument('-v', '--visualization', nargs=1, choices=['wordcloud', 'terminal', 'co_occurrence_matrix', 'correlation_circle'],
+                        default=["wordcloud"], type = str, help="Type of visualization: wordcloud, correlation matrix, correlation circle")
     parser.add_argument('-q', '--query', nargs=1,
                         help="Tweet query. Default is the first trending topic in New York")
     parser.add_argument('-n', '--number', nargs=1, type=int, default=10,
@@ -89,7 +109,7 @@ def main():
     if "query" in args and args.query != None:
         toSearch = args.query
     tweets = api.get_tweets(toSearch, args.number)
-    if args.type == "all":
+    if args.type[0] == "all":
         tweets_text = [tweet["text"] for tweet in tweets]
     else:
         tweets_text = [tweet["text"]
@@ -98,12 +118,19 @@ def main():
                           flags=re.MULTILINE) for t in tweets_text]
     tweets_text = [re.sub(r"RT", "", t, flags=re.MULTILINE)
                    for t in tweets_text]
-    
+
     if args.visualization[0] == "wordcloud":
         plot_wordcloud(tweets_text, args.type[0])
-    elif args.visualization[0] == "correlation_circle":
+    else:
         tweets_tokenized = [tokenize(tweet) for tweet in tweets_text]
-        print(tweets_tokenized)
+        coOccurrenceMatrix = tokensToMatrix(tweets_tokenized)
+        data = pd.DataFrame.from_dict(coOccurrenceMatrix)
+        if args.visualization[0] == "terminal":
+            print(data)
+        elif args.visualization[0] == "co_occurrence_matrix":
+            pass
+        elif args.visualization[0] == "correlation_circle":
+            pass
 
 if __name__ == "__main__":
     # calling main function
